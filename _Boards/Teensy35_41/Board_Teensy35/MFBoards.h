48992b96e1d029fe36942338a32fa36d023ae874
elral <3263285+elral@users.noreply.github.com>
2021-11-29 15:35:00 +0100

    support for Teensy35/41 added

---
+++ _Boards/Teensy35_41/Board_Teensy35/MFBoards.h
@@ -0,0 +1,38 @@
+#ifndef MFBoardTeensy35_h
+#define MFBoardTeensy35_h
+
+#define MF_SEGMENT_SUPPORT  1
+#define MF_LCD_SUPPORT      1
+#define MF_STEPPER_SUPPORT  1
+#define MF_SERVO_SUPPORT    1
+#define MF_JOYSTICK_SUPPORT 1
+#define MF_ANALOG_SUPPORT   1
+#define MF_SHIFTER_SUPPORT  1
+
+#define MAX_OUTPUTS         40
+#define MAX_BUTTONS         56
+#define MAX_LEDSEGMENTS     4
+#define MAX_ENCODERS        15
+#define MAX_STEPPERS        10
+#define MAX_MFSERVOS        10
+#define MAX_MFLCD_I2C       2
+#define MAX_ANALOG_INPUTS   5       // check for warning!!
+#define MAX_SHIFTERS        10
+#define MAX_MCP_EXPANDER    4
+
+#define STEPS               64
+#define STEPPER_SPEED       400     // 300 already worked, 467, too?
+#define STEPPER_ACCEL       800
+
+#define NATIVE_MAX_PINS     57      // max Pin number! from module, w/o Port Expander
+#define MCP_PIN_BASE        100     // first Pin number from Port Expander
+#define MODULE_MAX_PINS     NATIVE_MAX_PINS
+
+#define MOBIFLIGHT_TYPE         "MobiFlight Teensy35"
+#define MOBIFLIGHT_SERIAL       "1234567890"
+#define MOBIFLIGHT_NAME         "MobiFlight Teensy35"
+#define EEPROM_SIZE             4096    // EEPROMSizeTeensy3
+#define MEMLEN_CONFIG           1024     // MUST be less than EEPROM_SIZE!! MEM_OFFSET_CONFIG + MEM_LEN_CONFIG <= EEPROM_SIZE, see: eeprom_write_block (MEM_OFFSET_CONFIG, configBuffer, MEM_LEN_CONFIG);
+#define RANDOM_SEED_INPUT       A0
+
+#endif
---
+++ _Boards/Teensy35_41/MFEEPROM.cpp
@@ -0,0 +1,166 @@
+#include <Arduino.h>
+#include "MFBoards.h"
+#include "MFEEPROM.h"
+#include <EEPROM.h>
+
+MFEEPROM::MFEEPROM() {}
+
+void MFEEPROM::get_length(void) {
+    eepromLength = EEPROM.length();
+}
+
+void MFEEPROM::read_block(uint16_t adr, char data[], uint16_t len) {
+    for (uint16_t i = 0; i<len; i++) {
+        data[i] = read_char(adr + i);
+    }
+}
+
+void MFEEPROM::init() {
+    eepromLength = EEPROM.length();
+}
+
+void MFEEPROM::write_block (uint16_t adr, char data[], uint16_t len) {
+    if (adr + len >= eepromLength) return;
+    for (uint16_t i = 0; i<len; i++) {
+        EEPROM.put(adr + i,data[i]);    // kann das nicht auch write sein??
+    }
+}
+
+char MFEEPROM::read_char(uint16_t adr) {
+    if (adr >= eepromLength) return 0;
+    return EEPROM.read(adr);
+}
+
+void MFEEPROM::write_byte (uint16_t adr, char data) {
+    if (adr >= eepromLength) return;
+    EEPROM.put(adr, data);              // kann das nicht auch write sein??
+}
+
+
+/*
+
+#if defined(ARDUINO_ARCH_AVR) || defined(CORE_TEENSY) || defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_STM32F1)
+
+void eeprom_init(void) {
+#if defined(ARDUINO_ARCH_RP2040)
+    EEPROM.begin(EEPROM_SIZE);
+#endif
+# if !defined(ARDUINO_ARCH_STM32F1)
+    eepromLength = EEPROM.length();
+#else
+    eepromLength = EEPROM_SIZE;
+#endif
+    return;
+}
+
+void eeprom_read_block(uint16_t adr, char data[], uint16_t len) {
+    if (adr + len >= eepromLength) return;
+    for (uint16_t i = 0; i<len; i++) {
+        data[i] = EEPROM.read(adr+i);
+    }
+}
+
+void eeprom_write_block (uint16_t adr, char data[], uint16_t len) {
+    if (adr + len >= eepromLength) return;
+    for (uint16_t i = 0; i<len; i++) {
+#if defined(ARDUINO_ARCH_STM32F1)
+        EEPROM.write(adr + i,data[i]);
+#else
+        EEPROM.put(adr + i,data[i]);
+#endif
+    }
+#if defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_ESP8266)
+    EEPROM.commit();
+#endif
+}
+
+char eeprom_read_char(uint16_t adr) {
+    if (adr >= eepromLength) return 0;
+    return EEPROM.read(adr);
+}
+
+void eeprom_write_byte (uint16_t adr, char data) {
+    if (adr >= eepromLength) return;
+#if defined(ARDUINO_ARCH_STM32F1)
+    EEPROM.write(adr,data);
+#else
+    EEPROM.put(adr, data);
+#endif
+#if defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_ESP8266)
+    EEPROM.commit();
+#endif
+}
+
+
+#elif defined(ARDUINO_ARCH_STM32)
+
+#if defined(DATA_EEPROM_BASE)
+# warning  "STM32 w/o buffered API"
+
+void eeprom_init(void) {
+        EEPROM.init();
+        eepromLength = EEPROM.length();
+}
+
+void eeprom_read_block(uint16_t adr, char data[], uint16_t len) {
+    if (adr + len >= eepromLength) return;
+    for (uint16_t i = 0; i<len; i++) {
+        data[i] = EEPROM.read(adr + i);
+    }
+}
+
+void eeprom_write_block (uint16_t adr, char data[], uint16_t len) {
+    if (adr + len >= eepromLength) return;
+    for (uint16_t i = 0; i<len; i++) {
+        EEPROM.write(adr + i,data[i]);
+    }
+}
+
+uint8_t eeprom_read_char(uint16_t adr, char data) {
+    if (adr >= eepromLength) return 0;
+    data = EEPROM.read(adr);
+}
+
+void eeprom_write_byte (uint16_t adr, char data) {
+    if (adr >= eepromLength) return;
+    EEPROM.write(adr, data);
+}
+
+#else
+
+# warning  "STM32 with buffered API"
+
+void eeprom_init(void) {
+        eeprom_buffer_fill();
+        eepromLength = EEPROM.length();
+}
+
+void eeprom_read_block(uint16_t adr, char data[], uint16_t len) {
+    if (adr + len >= eepromLength) return;
+    for (uint16_t i = 0; i<len; i++) {
+        data[i] = eeprom_buffered_read_byte(adr + i);
+    }
+}
+
+void eeprom_write_block (uint16_t adr, char data[], uint16_t len) {
+    if (adr + len >= eepromLength) return;
+    for (uint16_t i = 0; i<len; i++) {
+        eeprom_buffered_write_byte(adr + i,data[i]);
+    }
+    eeprom_buffer_flush();
+}
+
+char eeprom_read_char(uint16_t adr) {
+    if (adr >= eepromLength) return 0;
+    return eeprom_buffered_read_byte(adr);
+}
+
+void eeprom_write_byte (uint16_t adr, char data) {
+    if (adr >= eepromLength) return;
+    EEPROM.write(adr, data);
+    eeprom_buffer_flush();
+}
+#endif
+
+#endif
+*/
