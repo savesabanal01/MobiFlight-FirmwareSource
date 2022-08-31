b86973c07827d0ced575c1ba99657b69e4b5f730
elral <3263285+elral@users.noreply.github.com>
2022-08-31 10:06:41 +0200

    increase memory size for devices of pico

--- _Boards/RaspberryPi/pico/MFBoards.h
+++ _Boards/RaspberryPi/pico/MFBoards.h
@@ -52,7 +52,7 @@
 #define EEPROM_SIZE             4096    // EEPROMSizeRaspberryPico
 #define MEMLEN_CONFIG           1496    // MUST be less than EEPROM_SIZE!! MEM_OFFSET_CONFIG + MEM_LEN_CONFIG <= EEPROM_SIZE, see: eeprom_write_block (MEM_OFFSET_CONFIG, configBuffer, MEM_LEN_CONFIG);
 #define MEMLEN_NAMES_BUFFER     1000    // max. size for configBuffer, contains only names from inputs
-#define MF_MAX_DEVICEMEM        1500    // max. memory size for devices
+#define MF_MAX_DEVICEMEM        2000    // max. memory size for devices
 
 #define RANDOM_SEED_INPUT   A0      // better to have randomSeed(analogRead(RANDOM_SEED_INPUT)); in generateSerial(), "0" is not valid for all HW platforms
 
