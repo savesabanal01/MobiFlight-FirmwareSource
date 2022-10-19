61a24c34bd82d57eb26ad6d76d366fd563d0cd42
elral <3263285+elral@users.noreply.github.com>
2022-10-19 10:07:20 +0200

    optimized TFT settings

--- src/core2.cpp
+++ src/core2.cpp
@@ -63,13 +63,21 @@ circle_t *circle = new circle_param;
 // #########################################################################
 void init_TFT()
 {
-    // reduce systemfrequency to 125MHz to get max SPI speed (max. 62.5 MHz)
-    // higher systemfrequency will reduce SPI speed as it's limited to 62.5MHz
-    set_sys_clock_khz(125000, false);
+// #########################################################################
+//  reduce systemfrequency to 125MHz to get max SPI speed (max. 62.5 MHz)
+//  higher systemfrequency will reduce SPI speed as it's limited to 62.5MHz
+//  BUT! changing to 125MHz or using PIO for SPI disables servo functionality
+//  if servo is not needed, uncomment the following function
+//  this will speed up SPI transfer
+//  For this example 24.7fps will be increased to 40fps
+//  Using PIO for SPI will additionally free up the MISO pin
+//  running on 133MHz gives 26.2fps, running on 125MHz gives 40.3fps
+// #########################################################################
+    //set_sys_clock_khz(125000, false);
+
     // Changing SPI frequency to clk_sys/2
-    //uint32_t  freq = clock_get_hz(clk_sys);
     // clk_peri does not have a divider, so in and out frequencies must be the same
-    //clock_configure(clk_peri, 0, CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS, freq, freq);
+    clock_configure(clk_peri, 0, CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS, clock_get_hz(clk_sys), clock_get_hz(clk_sys));
 
     tft.init();
     tft.initDMA();
