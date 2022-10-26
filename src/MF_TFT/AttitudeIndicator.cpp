6d9d5931bef1ca177918467a33a888465f6bef0b
elral <3263285+elral@users.noreply.github.com>
2022-10-26 13:12:58 +0200

    scale via sprite

--- src/MF_TFT/AttitudeIndicator.cpp
+++ src/MF_TFT/AttitudeIndicator.cpp
@@ -13,24 +13,27 @@
 // Note: for a 240 * 320 area this is 150 Kbytes!
 #define SPRITE_WIDTH  240
 #define SPRITE_HEIGTH 240
+#define SPRITE_X0     0  // upper left x position where to plot
+#define SPRITE_Y0     40 // upper left y position where to plot
 #define TFT_WIDTH     240
 #define TFT_HEIGTH    320
-#define REDRAW_DELAY  16             // minimum delay in milliseconds between display updates
-#define BROWN         ILI9341_ORANGE // 0x5140 // 0x5960
-#define SKY_BLUE      0x02B5         // 0x0318 //0x039B //0x34BF
+#define REDRAW_DELAY  16     // minimum delay in milliseconds between display updates
+#define BROWN         0xFD20 // 0x5140 // 0x5960 the other are not working??
+#define SKY_BLUE      0x02B5 // 0x0318 //0x039B //0x34BF
 #define DARK_RED      0x8000
 #define DARK_GREY     0x39C7
-#define XC            120 //
-#define YC            160 //
+#define XC            120
+#define YC            120
 #define HOR           400 // 130 //400 // Horizon vector line length ### was 172
 #define DEG2RAD       0.0174532925
+#define WAIT          10 // Pause in milliseconds to set refresh speed
 
 void updateHorizon(int roll, int pitch);
-void drawHorizon(int roll, int pitch);
-void drawInfo(void);
+void drawHorizon(int roll, int pitch, bool sel);
 int  rollGenerator(int maxroll);
 void testRoll(void);
 void testPitch(void);
+void drawScale(bool sel);
 
 int last_roll  = 0; // the whole horizon graphic
 int last_pitch = 0;
@@ -47,26 +50,37 @@ uint32_t redrawTime = 0;
 
 void init_AttitudeIndicator(void)
 {
+    tft.startWrite(); // TFT chip select held low permanently
+
+    spr[0].setRotation(0);
+    spr[1].setRotation(0);
+
     // Create the 2 sprites, each is half the size of the screen
     sprPtr[0] = (uint16_t *)spr[0].createSprite(SPRITE_WIDTH, SPRITE_HEIGTH / 2);
     sprPtr[1] = (uint16_t *)spr[1].createSprite(SPRITE_WIDTH, SPRITE_HEIGTH / 2);
-
     // Move the sprite 1 coordinate datum upwards half the screen height
     // so from coordinate point of view it occupies the bottom of screen
-    spr[1].setViewport(0, -SPRITE_HEIGTH / 2, SPRITE_WIDTH, SPRITE_HEIGTH);
+    spr[1].setViewport(0 /* SPRITE_X0 */, -SPRITE_HEIGTH / 2, SPRITE_WIDTH, SPRITE_HEIGTH);
 
     // Define text datum for each Sprite
     spr[0].setTextDatum(MC_DATUM);
     spr[1].setTextDatum(MC_DATUM);
 
-    tft.fillRect(XC - 120, YC - 160, 240, 160, SKY_BLUE);
-    tft.fillRect(XC - 120, YC, 240, 160, BROWN);
+    spr[0].fillRect(XC - SPRITE_WIDTH / 2, YC - SPRITE_HEIGTH / 2, SPRITE_WIDTH, SPRITE_HEIGTH / 2, SKY_BLUE);
+    spr[0].fillRect(XC - SPRITE_WIDTH / 2, YC, SPRITE_WIDTH, SPRITE_HEIGTH / 2, BROWN);
+    spr[1].fillRect(XC - SPRITE_WIDTH / 2, YC - SPRITE_HEIGTH / 2, SPRITE_WIDTH, SPRITE_HEIGTH / 2, SKY_BLUE);
+    spr[1].fillRect(XC - SPRITE_WIDTH / 2, YC, SPRITE_WIDTH, SPRITE_HEIGTH / 2, BROWN);
 
     // Draw the horizon graphic
-    drawHorizon(0, 0);
-    drawInfo();
+    drawHorizon(0, 0, 0);
+    drawHorizon(0, 0, 1);
 
-    tft.startWrite(); // TFT chip select held low permanently
+    // Draw fixed text at top/bottom of screen
+    tft.setTextColor(TFT_YELLOW);
+    tft.setTextDatum(TC_DATUM); // Centre middle justified
+    tft.drawString("SPD  LNAV WNAV PTH", XC, 1, 1);
+    tft.drawString("Bodmer's AHI", XC, TFT_HEIGTH - 9, 1);
+
     startMillis = millis();
 }
 
@@ -85,12 +99,13 @@ int pitch = 0;
 
 void loop_AttitudeIndicator()
 {
-    tft.setTextColor(TFT_YELLOW, SKY_BLUE);
+    tft.setTextColor(TFT_YELLOW, TFT_BLACK);
     tft.setTextDatum(TC_DATUM); // Centre middle justified
     tft.drawString("Random", XC, 10, 1);
 
     // Refresh the display at regular intervals
-    if (millis() > redrawTime) {
+    //    if (millis() > redrawTime)
+    {
         redrawTime = millis() + REDRAW_DELAY;
 
         // Roll is in degrees in range +/-180
@@ -139,8 +154,8 @@ void updateHorizon(int roll, int pitch)
             if (pitch_error > 1) delta_pitch *= 2;
         }
 
-        drawHorizon(last_roll + delta_roll, last_pitch + delta_pitch);
-        drawInfo();
+        drawHorizon(last_roll + delta_roll, last_pitch + delta_pitch, 0);
+        drawHorizon(last_roll + delta_roll, last_pitch + delta_pitch, 1);
     }
 }
 
@@ -153,9 +168,9 @@ void swap(int16_t *a, int16_t *b)
     *b           = *a;
     *a           = temp;
 }
-void drawHorizon(int roll, int pitch)
+
+void drawHorizon(int roll, int pitch, bool sel)
 {
-    tft.setAddrWindow(XC - 100, YC - 100, 200, 200);
     // Calculate coordinates for line start
     float sx = cos(roll * DEG2RAD);
     float sy = sin(roll * DEG2RAD);
@@ -188,101 +203,96 @@ void drawHorizon(int roll, int pitch)
     if ((roll != last_roll) || (pitch != last_pitch)) {
         xdn = 6 * xd;
         ydn = 6 * yd;
-        tft.drawLine(XC - x0 - xdn, YC - y0 - ydn - pitch, XC + x0 - xdn, YC + y0 - ydn - pitch, SKY_BLUE);
-        tft.drawLine(XC - x0 + xdn, YC - y0 + ydn - pitch, XC + x0 + xdn, YC + y0 + ydn - pitch, BROWN);
+        spr[sel].drawLine(XC - x0 - xdn, YC - y0 - ydn - pitch, XC + x0 - xdn, YC + y0 - ydn - pitch, SKY_BLUE);
+        spr[sel].drawLine(XC - x0 + xdn, YC - y0 + ydn - pitch, XC + x0 + xdn, YC + y0 + ydn - pitch, BROWN);
         xdn = 5 * xd;
         ydn = 5 * yd;
-        tft.drawLine(XC - x0 - xdn, YC - y0 - ydn - pitch, XC + x0 - xdn, YC + y0 - ydn - pitch, SKY_BLUE);
-        tft.drawLine(XC - x0 + xdn, YC - y0 + ydn - pitch, XC + x0 + xdn, YC + y0 + ydn - pitch, BROWN);
+        spr[sel].drawLine(XC - x0 - xdn, YC - y0 - ydn - pitch, XC + x0 - xdn, YC + y0 - ydn - pitch, SKY_BLUE);
+        spr[sel].drawLine(XC - x0 + xdn, YC - y0 + ydn - pitch, XC + x0 + xdn, YC + y0 + ydn - pitch, BROWN);
         xdn = 4 * xd;
         ydn = 4 * yd;
-        tft.drawLine(XC - x0 - xdn, YC - y0 - ydn - pitch, XC + x0 - xdn, YC + y0 - ydn - pitch, SKY_BLUE);
-        tft.drawLine(XC - x0 + xdn, YC - y0 + ydn - pitch, XC + x0 + xdn, YC + y0 + ydn - pitch, BROWN);
+        spr[sel].drawLine(XC - x0 - xdn, YC - y0 - ydn - pitch, XC + x0 - xdn, YC + y0 - ydn - pitch, SKY_BLUE);
+        spr[sel].drawLine(XC - x0 + xdn, YC - y0 + ydn - pitch, XC + x0 + xdn, YC + y0 + ydn - pitch, BROWN);
 
         xdn = 3 * xd;
         ydn = 3 * yd;
-        tft.drawLine(XC - x0 - xdn, YC - y0 - ydn - pitch, XC + x0 - xdn, YC + y0 - ydn - pitch, SKY_BLUE);
-        tft.drawLine(XC - x0 + xdn, YC - y0 + ydn - pitch, XC + x0 + xdn, YC + y0 + ydn - pitch, BROWN);
+        spr[sel].drawLine(XC - x0 - xdn, YC - y0 - ydn - pitch, XC + x0 - xdn, YC + y0 - ydn - pitch, SKY_BLUE);
+        spr[sel].drawLine(XC - x0 + xdn, YC - y0 + ydn - pitch, XC + x0 + xdn, YC + y0 + ydn - pitch, BROWN);
     }
     xdn = 2 * xd;
     ydn = 2 * yd;
-    tft.drawLine(XC - x0 - xdn, YC - y0 - ydn - pitch, XC + x0 - xdn, YC + y0 - ydn - pitch, SKY_BLUE);
-    tft.drawLine(XC - x0 + xdn, YC - y0 + ydn - pitch, XC + x0 + xdn, YC + y0 + ydn - pitch, BROWN);
+    spr[sel].drawLine(XC - x0 - xdn, YC - y0 - ydn - pitch, XC + x0 - xdn, YC + y0 - ydn - pitch, SKY_BLUE);
+    spr[sel].drawLine(XC - x0 + xdn, YC - y0 + ydn - pitch, XC + x0 + xdn, YC + y0 + ydn - pitch, BROWN);
 
-    tft.drawLine(XC - x0 - xd, YC - y0 - yd - pitch, XC + x0 - xd, YC + y0 - yd - pitch, SKY_BLUE);
-    tft.drawLine(XC - x0 + xd, YC - y0 + yd - pitch, XC + x0 + xd, YC + y0 + yd - pitch, BROWN);
+    spr[sel].drawLine(XC - x0 - xd, YC - y0 - yd - pitch, XC + x0 - xd, YC + y0 - yd - pitch, SKY_BLUE);
+    spr[sel].drawLine(XC - x0 + xd, YC - y0 + yd - pitch, XC + x0 + xd, YC + y0 + yd - pitch, BROWN);
 
-    tft.drawLine(XC - x0, YC - y0 - pitch, XC + x0, YC + y0 - pitch, TFT_WHITE);
+    spr[sel].drawLine(XC - x0, YC - y0 - pitch, XC + x0, YC + y0 - pitch, TFT_WHITE);
 
-    last_roll  = roll;
-    last_pitch = pitch;
-    /*
-        for (uint16_t i = 99; i < 160; i++)
-        {
-          tft.drawCircle(XC, YC, i, ILI9341_BLACK);
-          //drawPieSlice(XC, YC, i, ILI9341_BLACK, 0, 180);
-        }
-    */
-    tft.setAddrWindow(0, 0, SPRITE_WIDTH, SPRITE_HEIGTH);
+    if (sel) {
+        last_roll  = roll;
+        last_pitch = pitch;
+    }
+/*
+    for (uint16_t i = 99; i < 170; i++) {
+       spr[sel].drawCircle(XC, YC, i, TFT_BLACK);
+    }
+*/
+    drawScale(sel);
+
+    tft.pushImageDMA(SPRITE_X0, SPRITE_Y0 + (SPRITE_HEIGTH / 2) * sel, SPRITE_WIDTH, SPRITE_HEIGTH / 2, sprPtr[sel]);
 }
 
 // #########################################################################
 // Draw the information
 // #########################################################################
-
-void drawInfo(void)
+void drawScale(bool sel)
 {
     // Update things near middle of screen first (most likely to get obscured)
 
     // Level wings graphic
-    tft.fillRect(XC - 1, YC - 1, 3, 3, TFT_RED);
-    tft.drawFastHLine(XC - 30, YC, 24, TFT_RED);
-    tft.drawFastHLine(XC + 30 - 24, YC, 24, TFT_RED);
-    tft.drawFastVLine(XC - 30 + 24, YC, 3, TFT_RED);
-    tft.drawFastVLine(XC + 30 - 24, YC, 3, TFT_RED);
+    spr[sel].fillRect(XC - 1, YC - 1, 3, 3, TFT_RED);
+    spr[sel].drawFastHLine(XC - 30, YC, 24, TFT_RED);
+    spr[sel].drawFastHLine(XC + 30 - 24, YC, 24, TFT_RED);
+    spr[sel].drawFastVLine(XC - 30 + 24, YC, 3, TFT_RED);
+    spr[sel].drawFastVLine(XC + 30 - 24, YC, 3, TFT_RED);
 
     // Pitch scale
-    tft.drawFastHLine(XC - 12, YC - 40, 24, TFT_WHITE);
-    tft.drawFastHLine(XC - 6, YC - 30, 12, TFT_WHITE);
-    tft.drawFastHLine(XC - 12, YC - 20, 24, TFT_WHITE);
-    tft.drawFastHLine(XC - 6, YC - 10, 12, TFT_WHITE);
+    spr[sel].drawFastHLine(XC - 12, YC - 40, 24, TFT_WHITE);
+    spr[sel].drawFastHLine(XC - 6, YC - 30, 12, TFT_WHITE);
+    spr[sel].drawFastHLine(XC - 12, YC - 20, 24, TFT_WHITE);
+    spr[sel].drawFastHLine(XC - 6, YC - 10, 12, TFT_WHITE);
 
-    tft.drawFastHLine(XC - 6, YC + 10, 12, TFT_WHITE);
-    tft.drawFastHLine(XC - 12, YC + 20, 24, TFT_WHITE);
-    tft.drawFastHLine(XC - 6, YC + 30, 12, TFT_WHITE);
-    tft.drawFastHLine(XC - 12, YC + 40, 24, TFT_WHITE);
+    spr[sel].drawFastHLine(XC - 6, YC + 10, 12, TFT_WHITE);
+    spr[sel].drawFastHLine(XC - 12, YC + 20, 24, TFT_WHITE);
+    spr[sel].drawFastHLine(XC - 6, YC + 30, 12, TFT_WHITE);
+    spr[sel].drawFastHLine(XC - 12, YC + 40, 24, TFT_WHITE);
 
     // Pitch scale values
-    tft.setTextColor(TFT_WHITE);
-    tft.setCursor(XC - 12 - 13, YC - 20 - 3);
-    tft.print("10");
-    tft.setCursor(XC + 12 + 1, YC - 20 - 3);
-    tft.print("10");
-    tft.setCursor(XC - 12 - 13, YC + 20 - 3);
-    tft.print("10");
-    tft.setCursor(XC + 12 + 1, YC + 20 - 3);
-    tft.print("10");
+    spr[sel].setTextColor(TFT_WHITE);
+    spr[sel].setCursor(XC - 12 - 13, YC - 20 - 3);
+    spr[sel].print("10");
+    spr[sel].setCursor(XC + 12 + 1, YC - 20 - 3);
+    spr[sel].print("10");
+    spr[sel].setCursor(XC - 12 - 13, YC + 20 - 3);
+    spr[sel].print("10");
+    spr[sel].setCursor(XC + 12 + 1, YC + 20 - 3);
+    spr[sel].print("10");
 
-    tft.setCursor(XC - 12 - 13, YC - 40 - 3);
-    tft.print("20");
-    tft.setCursor(XC + 12 + 1, YC - 40 - 3);
-    tft.print("20");
-    tft.setCursor(XC - 12 - 13, YC + 40 - 3);
-    tft.print("20");
-    tft.setCursor(XC + 12 + 1, YC + 40 - 3);
-    tft.print("20");
+    spr[sel].setCursor(XC - 12 - 13, YC - 40 - 3);
+    spr[sel].print("20");
+    spr[sel].setCursor(XC + 12 + 1, YC - 40 - 3);
+    spr[sel].print("20");
+    spr[sel].setCursor(XC - 12 - 13, YC + 40 - 3);
+    spr[sel].print("20");
+    spr[sel].setCursor(XC + 12 + 1, YC + 40 - 3);
+    spr[sel].print("20");
 
     // Display justified roll value near bottom of screen
-    tft.setTextColor(TFT_YELLOW, BROWN); // Text with background
-    tft.setTextDatum(MC_DATUM);          // Centre middle justified
-    tft.setTextPadding(24);              // Padding width to wipe previous number
+    tft.setTextColor(TFT_YELLOW, TFT_BLACK); // Text with background
+    tft.setTextDatum(MC_DATUM);              // Centre middle justified
+    tft.setTextPadding(24);                  // Padding width to wipe previous number
     tft.drawNumber(last_roll, XC, TFT_HEIGTH - 18, 1);
-
-    // Draw fixed text at bottom of screen
-    tft.setTextColor(TFT_YELLOW);
-    tft.setTextDatum(TC_DATUM); // Centre middle justified
-    tft.drawString("SPD  LNAV WNAV PTH", XC, 1, 1);
-    tft.drawString("Bodmer's AHI", XC, TFT_HEIGTH - 9, 1);
 }
 
 // #########################################################################
