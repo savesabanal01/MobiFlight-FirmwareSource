64cbf88cc345c9483b0a79ca3f8f8d29968da8a5
elral <3263285+elral@users.noreply.github.com>
2022-11-01 08:05:58 +0100

    draw half filled circle in once

--- src/MF_TFT/TFT.h
+++ src/MF_TFT/TFT.h
@@ -11,7 +11,8 @@ namespace TFT {
 void init();
 void setClippingArea(int32_t ClippingX0, int32_t ClippingY0, int32_t ClippingXwidth, int32_t ClippingYwidth, int32_t ClippingRadius);
 void drawLine(int32_t x0, int32_t y0, int32_t x1, int32_t y1, uint32_t color, bool sel);
-void fillHalfCircle(int32_t x0, int32_t y0, int32_t r, uint32_t color, bool upper, bool sel);
+void fillHalfCircleSprite(int32_t x0, int32_t y0, int32_t r, uint32_t colorUpper, uint32_t colorLower, bool sel);
+void fillHalfCircleTFT(int32_t x0, int32_t y0, int32_t r, uint32_t colorUpper, uint32_t colorLower);
 void drawPixel(int32_t x, int32_t y, uint32_t color, bool sel);
 void drawFastVLine(int32_t x, int32_t y, int32_t h, uint32_t color, bool sel);
 void drawFastHLine(int32_t x, int32_t y, int32_t w, uint32_t color, bool sel);
