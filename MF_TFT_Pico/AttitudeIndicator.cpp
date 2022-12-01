117dfc8bbf37b2e1bd8cb1d0cd754afd3f8ee4c6
elral <3263285+elral@users.noreply.github.com>
2022-12-01 13:27:36 +0100

    common defines w/ Lily, everything in sprite

--- MF_TFT_Pico/AttitudeIndicator.cpp
+++ MF_TFT_Pico/AttitudeIndicator.cpp
@@ -13,34 +13,29 @@
 //     SPRITE_WIDTH * SPRITE_HEIGTH * 2 bytes of RAM
 // Note: for a 240 * 320 area this is 150 Kbytes!
 
-// Hmhm, but an outer rand should not get changed
-// so dimension of 200 x 200 should be OK and
-// no rect clipping would be required...
-// It's also valid for the round clipping area
-// sprite must be max. 2 * Radius in x and y direction...
-// But adapt Sprite center position...
-#define WIDTH_RECT_INNER  200
-#define HEIGTH_RECT_INNER 280                   // with this dimensions 112.5kBytes are required
-#define X0_RECT_INNER     20                    // upper left x position where to plot
-#define Y0_RECT_INNER     20                    // upper left y position where to plot
-#define CENTER_X0_RECT    WIDTH_RECT_INNER / 2  // x mid point in sprite for instrument, complete drawing must be inside sprite
-#define CENTER_Y0_RECT    HEIGTH_RECT_INNER / 2 // y mid point in sprite for instrument, complete drawing must be inside sprite
-#define WIDTH_RECT_OUTER  240                   // width of outer part of instrument
-#define HEIGTH_RECT_OUTER 320                   // height of outer part of instrument
-#define SPRITE_DIM_RADIUS 120                   // dimension for x and y direction of sprite, including outer part
-#define SPRITE_X0_ROUND   0                     // upper left x position where to plot
-#define SPRITE_Y0_ROUND   40                    // upper left y position where to plot
-#define CENTER_X0_ROUND   SPRITE_DIM_RADIUS     // x mid point in sprite for instrument, complete drawing must be inside sprite
-#define CENTER_Y0_ROUND   SPRITE_DIM_RADIUS     // y mid point in sprite for instrument, complete drawing must be inside sprite
-#define OUTER_RADIUS      SPRITE_DIM_RADIUS     // radius of outer part of instrument
-#define INNER_RADIUS      99                    // radius of moving part of instrument
-#define HOR               350                   // Horizon vector line, length must be at least sqrt(WIDTH_RECT_INNER^2 + HEIGTH_RECT_INNER^2) = 344
-#define MAX_PITCH         100                   // Maximum pitch shouls be in range +/- 80 with HOR = 172, 20 steps = 10 degrees on drawn scale
-#define BROWN             0xFD20                // 0x5140 // 0x5960 the other are not working??
-#define SKY_BLUE          0x02B5                // 0x0318 //0x039B //0x34BF
-#define DARK_RED          TFT_RED               // 0x8000
-#define DARK_GREY         TFT_BLACK             // ILI9341_DARKGREY
-#define LIGHT_GREY        TFT_BLACK             // ILI9341_LIGHTGREY
+#define X0_RECT           0                      // upper left x position where to plot
+#define Y0_RECT           20                     // upper left y position where to plot
+#define WIDTH_RECT_INNER  200                    // width of inner moving rect instrument
+#define HEIGTH_RECT_INNER 240                    // height of inner moving rect instrument
+#define WIDTH_RECT_OUTER  240                    // width of outer part of instrument
+#define HEIGTH_RECT_OUTER 280                    // height of outer part of instrument
+#define CENTER_X0_RECT    WIDTH_RECT_OUTER / 2   // x mid point in sprite for instrument, complete drawing must be inside sprite
+#define CENTER_Y0_RECT    HEIGTH_RECT_OUTER / 2  // y mid point in sprite for instrument, complete drawing must be inside sprite
+#define SPRITE_DIM_RADIUS 120                    // dimension for x and y direction of sprite, including outer part
+#define X0_ROUND          0                      // upper left x position where to plot
+#define Y0_ROUND          40                     // upper left y position where to plot
+#define CENTER_X0_ROUND   SPRITE_DIM_RADIUS      // x mid point in sprite for instrument, complete drawing must be inside sprite
+#define CENTER_Y0_ROUND   SPRITE_DIM_RADIUS      // y mid point in sprite for instrument, complete drawing must be inside sprite
+#define OUTER_RADIUS      SPRITE_DIM_RADIUS      // radius of outer part of instrument
+#define INNER_RADIUS      SPRITE_DIM_RADIUS - 21 // radius of moving part of instrument
+#define HOR_RECT          350                    // Horizon vector line, length must be at least sqrt(WIDTH_RECT_INNER^2 + HEIGTH_RECT_INNER^2) + MAX_PITCH = 520
+#define HOR_ROUND         350                    // Horizon vector line, length must be at least sqrt(2)*INNER_RADIUS + MAX_PITCH = 520
+#define MAX_PITCH         100                    // Maximum pitch shouls be in range +/- 80 with HOR = 172, 20 steps = 10 degrees on drawn scale
+#define BROWN             0xFD20                 // 0x5140 // 0x5960 the other are not working??
+#define SKY_BLUE          0x02B5                 // 0x0318 //0x039B //0x34BF
+#define DARK_RED          TFT_RED                // 0x8000
+#define DARK_GREY         TFT_BLACK              // ILI9341_DARKGREY
+#define LIGHT_GREY        TFT_BLACK              // ILI9341_LIGHTGREY
 // TFT_TRANSPARENT check how to use
 // spr[0].fillSprite(TFT_TRANSPARENT);
 // spr[0].setColorDepth(int8_t b);
@@ -85,15 +80,15 @@ namespace AttitudeIndicator
             sprPtr[1] = (uint16_t *)spr[1].createSprite(SPRITE_DIM_RADIUS * 2, SPRITE_DIM_RADIUS);
             // Move the sprite 1 coordinate datum upwards half the screen height
             // so from coordinate point of view it occupies the bottom of screen
-            spr[1].setViewport(0 /* X0_RECT_INNER */, -SPRITE_DIM_RADIUS, SPRITE_DIM_RADIUS * 2, SPRITE_DIM_RADIUS * 2);
+            spr[1].setViewport(0, -SPRITE_DIM_RADIUS, SPRITE_DIM_RADIUS * 2, SPRITE_DIM_RADIUS * 2);
         }
         if (instrumentType == RECT_SHAPE) {
             // Create the 2 sprites, each is half the size of the screen
-            sprPtr[0] = (uint16_t *)spr[0].createSprite(WIDTH_RECT_INNER, HEIGTH_RECT_INNER / 2);
-            sprPtr[1] = (uint16_t *)spr[1].createSprite(WIDTH_RECT_INNER, HEIGTH_RECT_INNER / 2);
+            sprPtr[0] = (uint16_t *)spr[0].createSprite(WIDTH_RECT_OUTER, HEIGTH_RECT_OUTER / 2);
+            sprPtr[1] = (uint16_t *)spr[1].createSprite(WIDTH_RECT_OUTER, HEIGTH_RECT_OUTER / 2);
             // Move the sprite 1 coordinate datum upwards half the screen height
             // so from coordinate point of view it occupies the bottom of screen
-            spr[1].setViewport(0 /* X0_RECT_INNER */, -HEIGTH_RECT_INNER / 2, WIDTH_RECT_INNER, HEIGTH_RECT_INNER);
+            spr[1].setViewport(0, -HEIGTH_RECT_OUTER / 2, WIDTH_RECT_OUTER, HEIGTH_RECT_OUTER);
         }
         // Define text datum for each Sprite
         spr[0].setTextDatum(MC_DATUM);
@@ -206,8 +201,14 @@ namespace AttitudeIndicator
     void drawHorizon(int roll, int pitch, bool sel)
     {
         // Calculate coordinates for line start for inner moving part
-        int16_t x0 = (float)cos(roll * DEG2RAD) * HOR;
-        int16_t y0 = (float)sin(roll * DEG2RAD) * HOR;
+        int16_t x0, y0;
+        if (instrumentType == ROUND_SHAPE) {
+            x0 = (float)cos(roll * DEG2RAD) * HOR_ROUND;
+            y0 = (float)sin(roll * DEG2RAD) * HOR_ROUND;
+        } else {
+            x0 = (float)cos(roll * DEG2RAD) * HOR_RECT;
+            y0 = (float)sin(roll * DEG2RAD) * HOR_RECT;
+        }
         // Calculate coordinates for line start for outer part, roll has not to be considered
         int16_t x0outer = OUTER_RADIUS;
         int16_t y0outer = 0;
@@ -294,22 +295,23 @@ namespace AttitudeIndicator
                 // draw the scale
                 drawScale(sel);
 
-                tft.pushImageDMA(SPRITE_X0_ROUND, SPRITE_Y0_ROUND + (SPRITE_DIM_RADIUS)*sel, SPRITE_DIM_RADIUS * 2, SPRITE_DIM_RADIUS, sprPtr[sel]);
+                tft.pushImageDMA(X0_ROUND, Y0_ROUND + (SPRITE_DIM_RADIUS)*sel, SPRITE_DIM_RADIUS * 2, SPRITE_DIM_RADIUS, sprPtr[sel]);
             }
         }
         if (instrumentType == RECT_SHAPE) {
             if ((roll != last_roll) || (pitch != last_pitch)) {
                 // draw outer part
                 // left side
-                tft.fillRect(0, Y0_RECT_INNER, Y0_RECT_INNER - 2, (HEIGTH_RECT_OUTER / 2) - pitch - 1, SKY_BLUE);
-                tft.fillRect(0, Y0_RECT_INNER + (HEIGTH_RECT_OUTER / 2) - pitch, Y0_RECT_INNER - 2, (HEIGTH_RECT_OUTER / 2) + pitch, BROWN);
-                tft.drawFastHLine(0, Y0_RECT_INNER + (HEIGTH_RECT_OUTER / 2) - pitch, Y0_RECT_INNER - 2, TFT_WHITE);
-                // right side
-                tft.fillRect(Y0_RECT_INNER + WIDTH_RECT_INNER + 2, Y0_RECT_INNER, Y0_RECT_INNER - 2, (HEIGTH_RECT_OUTER / 2) - pitch - 1, SKY_BLUE);
-                tft.fillRect(Y0_RECT_INNER + WIDTH_RECT_INNER + 2, Y0_RECT_INNER + (HEIGTH_RECT_OUTER / 2) - pitch, Y0_RECT_INNER - 2, (HEIGTH_RECT_OUTER / 2) + pitch, BROWN);
-                tft.drawFastHLine(Y0_RECT_INNER + WIDTH_RECT_INNER + 2, Y0_RECT_INNER + (HEIGTH_RECT_OUTER / 2) - pitch, Y0_RECT_INNER - 2, TFT_WHITE);
+                spr[sel].fillRect(CENTER_X0_RECT - WIDTH_RECT_OUTER / 2, CENTER_Y0_RECT - HEIGTH_RECT_OUTER / 2, (WIDTH_RECT_OUTER - WIDTH_RECT_INNER) / 2 - 1, HEIGTH_RECT_OUTER / 2 - pitch, SKY_BLUE);
+                spr[sel].fillRect(CENTER_X0_RECT - WIDTH_RECT_OUTER / 2, CENTER_Y0_RECT - pitch, (WIDTH_RECT_OUTER - WIDTH_RECT_INNER) / 2 - 1, HEIGTH_RECT_OUTER / 2 + pitch, BROWN);
+                spr[sel].drawFastHLine(CENTER_X0_RECT - WIDTH_RECT_OUTER / 2, CENTER_Y0_RECT - pitch, (WIDTH_RECT_OUTER - WIDTH_RECT_INNER) / 2 - 1, TFT_WHITE);
+                // next right side
+                spr[sel].fillRect(CENTER_X0_RECT + WIDTH_RECT_INNER / 2 + 1, CENTER_Y0_RECT - HEIGTH_RECT_OUTER / 2, (WIDTH_RECT_OUTER - WIDTH_RECT_INNER) / 2 - 1, HEIGTH_RECT_OUTER / 2 - pitch, SKY_BLUE);
+                spr[sel].fillRect(CENTER_X0_RECT + WIDTH_RECT_INNER / 2 + 1, CENTER_Y0_RECT - pitch, (WIDTH_RECT_OUTER - WIDTH_RECT_INNER) / 2 - 1, HEIGTH_RECT_OUTER / 2 + pitch, BROWN);
+                spr[sel].drawFastHLine(CENTER_X0_RECT + WIDTH_RECT_INNER / 2 + 1, CENTER_Y0_RECT - pitch, (WIDTH_RECT_OUTER - WIDTH_RECT_INNER) / 2 - 1, TFT_WHITE);
+
                 // draw inner moving part
-                TFT::setClippingArea(CENTER_X0_RECT, CENTER_Y0_RECT, WIDTH_RECT_OUTER, HEIGTH_RECT_OUTER, 0, 0);
+                TFT::setClippingArea(CENTER_X0_RECT, CENTER_Y0_RECT, WIDTH_RECT_INNER, HEIGTH_RECT_INNER, 0, 0);
                 for (uint8_t i = 6; i > 0; i--) {
                     xdn   = i * xd;
                     ydn   = i * yd;
@@ -333,12 +335,11 @@ namespace AttitudeIndicator
                 // draw the scale
                 drawScale(sel);
 
-                tft.pushImageDMA(X0_RECT_INNER, Y0_RECT_INNER + (HEIGTH_RECT_INNER / 2) * sel, WIDTH_RECT_INNER, HEIGTH_RECT_INNER / 2, sprPtr[sel]);
                 // draw a border around the inner moving part
-                tft.drawRect(X0_RECT_INNER - 1, Y0_RECT_INNER - 1, WIDTH_RECT_INNER + 2, HEIGTH_RECT_INNER + 2, DARK_GREY);
-                tft.drawRect(X0_RECT_INNER - 1, Y0_RECT_INNER - 1, WIDTH_RECT_INNER + 2, HEIGTH_RECT_INNER + 2, DARK_GREY);
-                tft.drawRect(X0_RECT_INNER - 2, Y0_RECT_INNER - 2, WIDTH_RECT_INNER + 4, HEIGTH_RECT_INNER + 4, DARK_GREY);
-                tft.drawRect(X0_RECT_INNER - 2, Y0_RECT_INNER - 2, WIDTH_RECT_INNER + 4, HEIGTH_RECT_INNER + 4, DARK_GREY);
+                spr[sel].drawRect(CENTER_X0_RECT - WIDTH_RECT_INNER / 2 - 0, CENTER_Y0_RECT - HEIGTH_RECT_INNER / 2 - 0, WIDTH_RECT_INNER + 0, HEIGTH_RECT_INNER + 0, DARK_GREY);
+                spr[sel].drawRect(CENTER_X0_RECT - WIDTH_RECT_INNER / 2 - 0, CENTER_Y0_RECT - HEIGTH_RECT_INNER / 2 - 0, WIDTH_RECT_INNER + 0, HEIGTH_RECT_INNER + 0, DARK_GREY);
+                spr[sel].drawRect(CENTER_X0_RECT - WIDTH_RECT_INNER / 2 - 1, CENTER_Y0_RECT - HEIGTH_RECT_INNER / 2 - 1, WIDTH_RECT_INNER + 2, HEIGTH_RECT_INNER + 2, DARK_GREY);
+                tft.pushImageDMA(X0_RECT, Y0_RECT + (HEIGTH_RECT_OUTER / 2) * sel, WIDTH_RECT_OUTER, HEIGTH_RECT_OUTER / 2, sprPtr[sel]);
             }
         }
     }
@@ -388,7 +389,7 @@ namespace AttitudeIndicator
             spr[sel].setCursor(CENTER_X0_ROUND + 12 + 1, CENTER_Y0_ROUND + 40 - 3);
             spr[sel].print("20");
 
-            tft.setTextColor(TFT_WHITE, TFT_BLACK); // Text with background
+            spr[sel].setTextColor(TFT_WHITE, TFT_BLACK); // Text with background
         }
         if (instrumentType == RECT_SHAPE) {
             // Update things near middle of screen first (most likely to get obscured)
@@ -429,7 +430,7 @@ namespace AttitudeIndicator
             spr[sel].print("20");
             spr[sel].setCursor(CENTER_X0_RECT + 12 + 1, CENTER_Y0_RECT + 40 - 3);
             spr[sel].print("20");
-            tft.setTextColor(TFT_BLACK, BROWN); // Text with background
+            spr[sel].setTextColor(TFT_BLACK, BROWN); // Text with background
         }
 
         // Display justified roll value near bottom of screen
@@ -447,18 +448,18 @@ namespace AttitudeIndicator
             TFT::fillHalfCircleSprite(CENTER_X0_ROUND, CENTER_Y0_ROUND, OUTER_RADIUS, SKY_BLUE, BROWN, 0);
             TFT::fillHalfCircleSprite(CENTER_X0_ROUND, CENTER_Y0_ROUND, OUTER_RADIUS, SKY_BLUE, BROWN, 1);
             // and now the "static" area
-            TFT::fillHalfCircleTFT(SPRITE_X0_ROUND + SPRITE_DIM_RADIUS, SPRITE_Y0_ROUND + SPRITE_DIM_RADIUS, OUTER_RADIUS, SKY_BLUE, BROWN);
-            tft.drawCircle(SPRITE_X0_ROUND + SPRITE_DIM_RADIUS, SPRITE_Y0_ROUND + SPRITE_DIM_RADIUS, OUTER_RADIUS, LIGHT_GREY);
+            TFT::fillHalfCircleTFT(X0_ROUND + SPRITE_DIM_RADIUS, Y0_ROUND + SPRITE_DIM_RADIUS, OUTER_RADIUS, SKY_BLUE, BROWN);
+            spr[0].drawCircle(CENTER_X0_ROUND, CENTER_Y0_ROUND, OUTER_RADIUS, LIGHT_GREY);
+            spr[1].drawCircle(CENTER_X0_ROUND, CENTER_Y0_ROUND, OUTER_RADIUS, LIGHT_GREY);
         }
         if (instrumentType == RECT_SHAPE) {
             // fill sprite with not moving area
-            spr[0].fillRect(CENTER_X0_RECT - WIDTH_RECT_INNER / 2, CENTER_Y0_RECT - HEIGTH_RECT_INNER / 2, WIDTH_RECT_INNER, HEIGTH_RECT_INNER / 2, SKY_BLUE);
-            spr[0].fillRect(CENTER_X0_RECT - WIDTH_RECT_INNER / 2, CENTER_Y0_RECT, WIDTH_RECT_INNER, HEIGTH_RECT_INNER / 2, BROWN);
-            spr[1].fillRect(CENTER_X0_RECT - WIDTH_RECT_INNER / 2, CENTER_Y0_RECT - HEIGTH_RECT_INNER / 2, WIDTH_RECT_INNER, HEIGTH_RECT_INNER / 2, SKY_BLUE);
-            spr[1].fillRect(CENTER_X0_RECT - WIDTH_RECT_INNER / 2, CENTER_Y0_RECT, WIDTH_RECT_INNER, HEIGTH_RECT_INNER / 2, BROWN);
-            // fill area outside sprite with not moving area
-            tft.fillRect(0, 0, TFT_WIDTH, TFT_HEIGHT / 2, SKY_BLUE);
-            tft.fillRect(0, 160, TFT_WIDTH, TFT_HEIGHT / 2, BROWN);
+            spr[0].fillRect(CENTER_X0_RECT - WIDTH_RECT_OUTER / 2, CENTER_Y0_RECT - HEIGTH_RECT_OUTER / 2, WIDTH_RECT_OUTER, HEIGTH_RECT_OUTER / 2, SKY_BLUE);
+            spr[0].fillRect(CENTER_X0_RECT - WIDTH_RECT_OUTER / 2, CENTER_Y0_RECT, WIDTH_RECT_OUTER, HEIGTH_RECT_OUTER / 2, BROWN);
+            spr[0].drawFastHLine(CENTER_X0_RECT - WIDTH_RECT_OUTER / 2 - 1, CENTER_Y0_RECT, WIDTH_RECT_OUTER - 2, TFT_WHITE);
+            spr[1].fillRect(CENTER_X0_RECT - WIDTH_RECT_OUTER / 2, CENTER_Y0_RECT - HEIGTH_RECT_OUTER / 2, WIDTH_RECT_OUTER, HEIGTH_RECT_OUTER / 2, SKY_BLUE);
+            spr[1].fillRect(CENTER_X0_RECT - WIDTH_RECT_OUTER / 2, CENTER_Y0_RECT, WIDTH_RECT_OUTER, HEIGTH_RECT_OUTER / 2, BROWN);
+            spr[1].drawFastHLine(CENTER_X0_RECT - WIDTH_RECT_OUTER / 2 - 1, CENTER_Y0_RECT, WIDTH_RECT_OUTER - 2, TFT_WHITE);
         }
         // Draw the horizon graphic
         drawHorizon(0, 0, 0);
