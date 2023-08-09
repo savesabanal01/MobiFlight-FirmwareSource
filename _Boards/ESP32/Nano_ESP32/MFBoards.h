2fe7e83fa055750d01acaac90874dc6818a9b98b
elral <3263285+elral@users.noreply.github.com>
2023-08-09 10:12:30 +0200

    comment updated

--- _Boards/ESP32/Nano_ESP32/MFBoards.h
+++ _Boards/ESP32/Nano_ESP32/MFBoards.h
@@ -45,7 +45,7 @@
 #define MOBIFLIGHT_TYPE         "MobiFlight Nano ESP32"
 #define MOBIFLIGHT_SERIAL       "0987654321"
 #define MOBIFLIGHT_NAME         "MobiFlight Nano ESP32"
-#define EEPROM_SIZE             4096    // EEPROMSizeRaspberryPico
+#define EEPROM_SIZE             4096    // EEPROMSizeNanoESP32
 #define MEMLEN_CONFIG           1496    // MUST be less than EEPROM_SIZE!! MEM_OFFSET_CONFIG + MEM_LEN_CONFIG <= EEPROM_SIZE, see: eeprom_write_block (MEM_OFFSET_CONFIG, configBuffer, MEM_LEN_CONFIG);
 #define MEMLEN_NAMES_BUFFER     1000    // max. size for configBuffer, contains only names from inputs
 #define MF_MAX_DEVICEMEM        2000    // max. memory size for devices
