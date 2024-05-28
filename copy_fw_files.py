80127a6212507525feed0e1c1be42af9bd779733
elral <3263285+elral@users.noreply.github.com>
2024-05-28 09:06:00 +0200

    merge branch main

--- copy_fw_files.py
+++ copy_fw_files.py
@@ -9,12 +9,14 @@ if firmware_version == "":
 firmware_version = firmware_version.lstrip("v")
 firmware_version = firmware_version.strip(".")
 
+platform = env.BoardConfig().get("platform", {})
+
 zip_file_name = 'Mobiflight-Connector'
 build_path = './_build'
 build_path_fw = build_path + '/firmware'
 build_path_json = build_path + '/Boards'
 distrubution_path = './_dist'
-board_folder = ['./_Boards/Atmel', './_Boards/RaspberryPi']
+board_folder = ['./_Boards/Atmel', './_Boards/RaspberryPi', './_Boards/ESP32']
 
 def copy_fw_files (source, target, env):
     fw_file_name=str(target[0])
@@ -24,7 +26,10 @@ def copy_fw_files (source, target, env):
     if os.path.exists(build_path_json) == False:
         os.makedirs(build_path_json)
 
-    if fw_file_name[-3:] == "bin":
+    if fw_file_name[-3:] == "bin"and platform == "https://github.com/maxgerhardt/platform-raspberrypi.git":
+        fw_file_name=fw_file_name[0:-3] + "uf2"
+
+    if fw_file_name[-3:] == "bin"and platform == "https://github.com/platformio/platform-espressif32.git":
         fw_file_name=fw_file_name[0:-3] + "uf2"
 
     # Copy build FW file
